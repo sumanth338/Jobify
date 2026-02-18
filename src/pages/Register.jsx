30d08f1a1491bfb257e574c1bbd66d90c2b2c394
sumanth338 <sumanthashwin000@gmail.com>
2026-02-18 20:59:00 +0530

    Enhance Register page layout with FormRow components and Wrapper; import index.css for styling. Add FormRow export to components index.

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow, Logo } from '../components'
 
 const Register = () => {
   return (
     <>
-    <div>Register</div>
-    <Link to="/login">login</Link>
+   <Wrapper>
+    <form className='form'>
+      <Logo/>
+      <h4>Register</h4>
+      <FormRow type="text" name="name" labelText="name"/>
+      <FormRow type="text" name="Last Name" labelText="Last Name"/>
+      <FormRow type="text" name="Location" labelText="Location"/>
+      <FormRow type="email" name="email" labelText="email"/>
+      <FormRow type="password" name="password" labelText="password"/>
+      <button type="submit" className="btn btn-block">submit</button>
+        <p>
+          Already a member?
+          <Link to="/login" className='member-btn'>Login</Link>
+        </p>
+    </form>
+   </Wrapper>
     </>
   )
 }
