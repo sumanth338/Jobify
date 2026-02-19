5ba39a19b8c04869470c41f6c048f80f1a4daf11
sumanth338 <sumanthashwin000@gmail.com>
2026-02-19 20:11:43 +0530

    Refactor Login page layout to use Wrapper and FormRow components; enhance user interface with structured form elements and navigation links.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage'
+import { FormRow, Logo } from '../components'
 
 const Login = () => {
   return (
     <>
-    <div>Login</div>
-    <Link to="/register">register</Link>
+    <Wrapper>
+      <form className='form'>
+        <Logo/>
+        <h4>Login</h4>
+        <FormRow type="email" name="email" labelText="email"/>
+        <FormRow type="password" name="password" labelText="password"/>
+        <button type="submit" className="btn btn-block">submit</button>
+        <button type="button" className="btn btn-block">explore the app</button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className='member-btn'>Register</Link>
+        </p>
+      </form>
+   
+    </Wrapper>
     </>
   )
-}
+};
 
-export default Login
+export default Login;
