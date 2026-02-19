2bdfc9f76659ced6819a2f437c1a2468dab9807c
sumanth338 <sumanthashwin000@gmail.com>
2026-02-19 20:12:52 +0530

    Refactor App component to include new dashboard routes and pages; enhance DashboardLayout with context for user state and sidebar management. Update components index to export additional sidebar and navbar components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomeLayout, Landing, Register, Login, DashboardLayout, Error } from './pages';
-
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+  AddJob,
+  Stats,
+  AllJobs,
+  Profile,
+  Admin,
+} from "./pages";
 
 const App = () => {
   return (
@@ -11,8 +22,15 @@ const App = () => {
           <Route index element={<Landing />} />
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
-          <Route path="dashboard" element={<DashboardLayout />} />
+          <Route path="dashboard" element={<DashboardLayout />}>
+            <Route index element={<AddJob />} />
+            <Route path="stats" element={<Stats />} />
+            <Route path="all-jobs" element={<AllJobs />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="admin" element={<Admin />} />
+          </Route>
         </Route>
+
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
--- src/pages/DashboardLayout.jsx
+++ src/pages/DashboardLayout.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { createContext, useContext, useState } from "react";
+import { Outlet } from "react-router-dom";
+import Wrapper from "../assets/wrappers/Dashboard";
+import { BigSidebar, Navbar, SmallSidebar } from "../components";
+
+const DashboardContext = createContext();
 
 const DashboardLayout = () => {
-  return (
-    <div>DashboardLayout</div>
-  )
-}
+  const user = {name: 'john'};
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-export default DashboardLayout
+  const toggleDarkTheme = ()=>{
+    console.log('toggle dark theme');
+  }
+  const toggleSidebar = ()=>{
+    setShowSidebar(!showSidebar)
+  }
+  const logoutUser = async ()=>{
+    console.log('logout user');
+  }
+  return (
+    <DashboardContext.Provider value={{ user, showSidebar, isDarkTheme, toggleDarkTheme, toggleSidebar, logoutUser }}>
+    <Wrapper>
+      <main className="dashboard">
+        <SmallSidebar />
+        <BigSidebar />
+        <div>
+          <Navbar />
+          <div className="dashboard-page">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </Wrapper>
+    </DashboardContext.Provider>
+  );
+};
+
+export const useDashboardContext = () => useContext(DashboardContext);
+export default DashboardLayout;
