da1980ce35479ecc3a4fb5e4bf4e2056c15557b6
sumanth338 <sumanthashwin000@gmail.com>
2026-02-23 21:35:38 +0530

    Implement Navbar and SmallSidebar components with toggle functionality; add links utility for navigation. Enhance UI with icons and structured layout.

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/SmallSidebar'
+import { FaTimes } from 'react-icons/fa'
+import Logo from './Logo'
+import links from '../utils/links'
+import { NavLink } from 'react-router-dom'
+import { useDashboardContext } from '../pages/DashboardLayout'
 // import { useDashboardContext } from '../pages/DashboardLayout'
 
 const SmallSidebar = () => {
     // const data = useDashboardContext();
     // console.log(data)
+    const {showSidebar, toggleSidebar} = useDashboardContext()
   return (
-    <Wrapper>SmallSidebar</Wrapper>
+    <Wrapper>
+      <div className={showSidebar ? "sidebar-container show-sidebar" :"sidebar-container"}>
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar} >
+            <FaTimes/>
+          </button>
+          <header>
+            <Logo/>
+          </header>
+          <div className="nav-links">
+            {links.map((link)=>{
+              const {text, path, icon} = link;
+              return(
+                <NavLink to={path} key={text} className="nav-link" onClick={toggleSidebar} end>
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+    </Wrapper>
   )
 }
 
