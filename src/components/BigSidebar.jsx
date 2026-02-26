e0bd5e4f42ffc30240d8e144ac429560bbe0ed49
sumanth338 <sumanthashwin000@gmail.com>
2026-02-26 14:15:18 +0530

    Refactor BigSidebar to integrate Navlinks and Logo components; enhance sidebar visibility management with context for improved user experience.

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Wrapper from '../assets/wrappers/BigSidebar'
+import Navlinks from './Navlinks' 
+import { useDashboardContext } from '../pages/DashboardLayout'
+import Logo from './Logo'
 
 const BigSidebar = () => {
+  const {showSidebar} = useDashboardContext()
   return (
     <Wrapper>
-      BigSidebar
+      <div className={showSidebar ? "sidebar-container" : "sidebar-container show-sidebar" }>
+      <div className="content">
+        <header>
+          <Logo/>
+        </header>
+        <Navlinks isBigSidebar/>
+      </div>
+      </div>
     </Wrapper>
   )
 }
--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,23 +1,29 @@
-import React from 'react'
-import links from '../utils/links'
-import { NavLink } from 'react-router-dom'
-import { useDashboardContext } from '../pages/DashboardLayout'
+import React from "react";
+import links from "../utils/links";
+import { NavLink } from "react-router-dom";
+import { useDashboardContext } from "../pages/DashboardLayout";
 
-const Navlinks = () => {
-    const {toggleSidebar, user} = useDashboardContext()
+const Navlinks = ({isBigSidebar}) => {
+  const { toggleSidebar, user } = useDashboardContext();
   return (
     <div className="nav-links">
-            {links.map((link)=>{
-              const {text, path, icon} = link;
-              return(
-                <NavLink to={path} key={text} className="nav-link" onClick={toggleSidebar} end>
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              )
-            })}
-          </div>
-  )
-}
+      {links.map((link) => {
+        const { text, path, icon } = link;
+        return (
+          <NavLink
+            to={path}
+            key={text}
+            className="nav-link"
+            onClick={isBigSidebar ? null : toggleSidebar}
+            end
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Navlinks
+export default Navlinks;
